8741394a40e676002c08b6f7f9d2f87e39234c82
insource <3248874747@qq.com>
2026-01-27 13:16:23 +0800

    first commit

---
+++ src/views/Home/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="home-container">
+    home
+    <img src="https://zooow-1258443890.cos.ap-guangzhou.myqcloud.com/3.png" alt="">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home-container {
+  width: 100vw;
+  height: 100vh;
+  background-color: #fff;
+}
+</style>
